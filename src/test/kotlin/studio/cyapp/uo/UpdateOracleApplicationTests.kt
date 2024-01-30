0219c1bf297ab221411d1d8b606e67a2c1960244
pmn <modirniya@live.com>
2024-01-30 23:17:14 +0330

    second commit

---
+++ src/test/kotlin/studio/cyapp/uo/UpdateOracleApplicationTests.kt
@@ -0,0 +1,13 @@
+package studio.cyapp.uo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UpdateOracleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
